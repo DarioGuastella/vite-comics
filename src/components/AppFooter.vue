8f6f7986715d0815f90a61c6826047bfa52d4b92
DarioGuastella <jimmyguastella@gmail.com>
2023-11-28 19:14:45 +0100

    aggiunge mappa sito

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,3 +1,15 @@
-<script></script>
-<template></template>
-<style></style>
+<script>
+import AppSiteMap from "../components/AppSiteMap.vue"
+export default {
+    components: {
+        AppSiteMap
+    },
+
+}
+</script>
+<template>
+    <footer>
+        <AppSiteMap />
+    </footer>
+</template>
+<style scoped></style>
---
+++ src/components/AppSiteMap.vue
@@ -0,0 +1,68 @@
+<script></script>
+<template>
+	<section class="container-fluid">
+
+		<div class="d-flex justify-content-around overflow-y-hidden">
+			<div class="d-flex p-5">
+				<ul>
+					<h5 class="text-white">DC COMICS</h5>
+					<li class="text-secondary">Characters</li>
+					<li class="text-secondary">Comics</li>
+					<li class="text-secondary">Movies</li>
+					<li class="text-secondary">TV</li>
+					<li class="text-secondary">Games</li>
+					<li class="text-secondary">Videos</li>
+					<li class="text-secondary">News</li>
+					<ul class="ps-0 pt-3">
+						<h5 class="text-white">SHOP</h5>
+						<li class="text-secondary">Shop DC</li>
+						<li class="text-secondary">Shop DC Collectibles</li>
+					</ul>
+				</ul>
+				<ul>
+					<h5 class="text-white">DC</h5>
+					<li class="text-secondary">Terms Of Use</li>
+					<li class="text-secondary">Privacy policy (New)</li>
+					<li class="text-secondary">Ad Choices</li>
+					<li class="text-secondary">Advertising</li>
+					<li class="text-secondary">Jobs</li>
+					<li class="text-secondary">Subscriptions</li>
+					<li class="text-secondary">Talent Workshops</li>
+					<li class="text-secondary">CPSC Certificates</li>
+					<li class="text-secondary">Ratings</li>
+					<li class="text-secondary">Shop Help</li>
+					<li class="text-secondary">Contact Us</li>
+				</ul>
+				<ul>
+					<h5 class="text-white">SITES</h5>
+					<li class="text-secondary">DC</li>
+					<li class="text-secondary">MAD Magazine</li>
+					<li class="text-secondary">DC Kids</li>
+					<li class="text-secondary">DC Universe</li>
+					<li class="text-secondary">DC Power Visa</li>
+				</ul>
+			</div>
+			<div class="">
+				<img src="../assets/img/dc-logo-bg.png" alt="">
+			</div>
+		</div>
+
+
+
+	</section>
+</template>
+<style scoped>
+section {
+	background-image: url("/src/assets/img/footer-bg.jpg");
+	min-height: 482px;
+	background-size: cover;
+}
+
+ul {
+	list-style-type: none;
+}
+
+img {
+	scale: 1.3;
+}
+</style>
