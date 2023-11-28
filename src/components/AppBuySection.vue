b8f5b1c57648bbe05887631c34407aa7cacfffa3
DarioGuastella <jimmyguastella@gmail.com>
2023-11-28 18:06:26 +0100

    sezione buy fix

--- src/components/AppBuySection.vue
+++ src/components/AppBuySection.vue
@@ -3,58 +3,72 @@
 </script>
 <template>
     <section id="buyRow">
-        <div class="container d-flex justify-content-center">
+        <div class="container d-flex justify-content-center p-5">
             <div class="d-flex align-items-center">
-                <div>
+                <div class="text-center pe-2">
                     <img src="../assets/img/buy-comics-digital-comics.png" alt="">
                 </div>
-                <div>
+                <div class="pe-5">
                     <span>DIGITAL COMICS</span>
                 </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div>
+
+
+                <div class="text-center pe-2">
                     <img src="../assets/img/buy-comics-merchandise.png" alt="">
                 </div>
-                <div>
+                <div class="pe-5">
                     <span>DC MERCHANDISE</span>
                 </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div>
+
+
+                <div class="text-center pe-2">
                     <img src="../assets/img/buy-comics-subscriptions.png" alt="">
                 </div>
-                <div>
+                <div class="pe-5">
                     <span>SUBSCRIPTION</span>
                 </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div>
+
+
+                <div class="text-center pe-2">
                     <img src="../assets/img/buy-comics-shop-locator.png" alt="">
                 </div>
-                <div>
+                <div class="pe-5">
                     <span>COMIC SHOP LOCATOR</span>
                 </div>
-            </div>
-            <div class="d-flex align-items-center">
-                <div>
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+
+
+                <div class="text-center pe-2">
+                    <img class="wideImg" src="../assets/img/buy-dc-power-visa.svg" alt="">
                 </div>
-                <div>
+                <div class="pe-5">
                     <span>DC POWER VISA</span>
                 </div>
-            </div>
 
+
+            </div>
         </div>
+
+
+
 
     </section>
 </template>
 <style scoped>
-p {
+span {
     margin: 0;
+    font-size: 0.8rem;
+}
+
+div img {
+    height: 60px;
+    width: 100%;
 }
 
 #buyRow {
     background-color: #0282f9;
+}
+
+.wideImg {
+    height: 47px;
 }
 </style>
