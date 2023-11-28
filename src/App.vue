07a05474808214c398e3877e9832ae28f9f19316
DarioGuastella <jimmyguastella@gmail.com>
2023-11-28 16:35:06 +0100

    imposta i files e le cartelle

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import AppFooter from "./components/AppFooter.vue"
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
+
+</script>
+
+<template>
+  <h1>prova</h1>
+</template>
+
+<style scoped></style>
