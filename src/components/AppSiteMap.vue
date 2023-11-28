050776549981cd901d34262d23c5c50bc8f0e3fb
DarioGuastella <jimmyguastella@gmail.com>
2023-11-28 19:43:20 +0100

    style: aggiunge link a mappa del sito

--- src/components/AppSiteMap.vue
+++ src/components/AppSiteMap.vue
@@ -6,40 +6,40 @@
 			<div class="d-flex p-5">
 				<ul>
 					<h5 class="text-white">DC COMICS</h5>
-					<li class="text-secondary">Characters</li>
-					<li class="text-secondary">Comics</li>
-					<li class="text-secondary">Movies</li>
-					<li class="text-secondary">TV</li>
-					<li class="text-secondary">Games</li>
-					<li class="text-secondary">Videos</li>
-					<li class="text-secondary">News</li>
+					<li class="text-secondary"><a href="#">Characters</a></li>
+					<li class="text-secondary"><a href="#">Comics</a></li>
+					<li class="text-secondary"><a href="#">Movies</a></li>
+					<li class="text-secondary"><a href="#">TV</a></li>
+					<li class="text-secondary"><a href="#">Games</a></li>
+					<li class="text-secondary"><a href="#">Videos</a></li>
+					<li class="text-secondary"><a href="#">News</a></li>
 					<ul class="ps-0 pt-3">
 						<h5 class="text-white">SHOP</h5>
-						<li class="text-secondary">Shop DC</li>
-						<li class="text-secondary">Shop DC Collectibles</li>
+						<li class="text-secondary"><a href="#">Shop DC</a></li>
+						<li class="text-secondary"><a href="#">Shop DC Collectibles</a></li>
 					</ul>
 				</ul>
 				<ul>
 					<h5 class="text-white">DC</h5>
-					<li class="text-secondary">Terms Of Use</li>
-					<li class="text-secondary">Privacy policy (New)</li>
-					<li class="text-secondary">Ad Choices</li>
-					<li class="text-secondary">Advertising</li>
-					<li class="text-secondary">Jobs</li>
-					<li class="text-secondary">Subscriptions</li>
-					<li class="text-secondary">Talent Workshops</li>
-					<li class="text-secondary">CPSC Certificates</li>
-					<li class="text-secondary">Ratings</li>
-					<li class="text-secondary">Shop Help</li>
-					<li class="text-secondary">Contact Us</li>
+					<li class="text-secondary"><a href="#">Terms Of Use</a></li>
+					<li class="text-secondary"><a href="#">Privacy policy (New)</a></li>
+					<li class="text-secondary"><a href="#">Ad Choices</a></li>
+					<li class="text-secondary"><a href="#">Advertising</a></li>
+					<li class="text-secondary"><a href="#">Jobs</a></li>
+					<li class="text-secondary"><a href="#">Subscriptions</a></li>
+					<li class="text-secondary"><a href="#">Talent Workshops</a></li>
+					<li class="text-secondary"><a href="#">CPSC Certificates</a></li>
+					<li class="text-secondary"><a href="#">Ratings</a></li>
+					<li class="text-secondary"><a href="#">Shop Help</a></li>
+					<li class="text-secondary"><a href="#">Contact Us</a></li>
 				</ul>
 				<ul>
 					<h5 class="text-white">SITES</h5>
-					<li class="text-secondary">DC</li>
-					<li class="text-secondary">MAD Magazine</li>
-					<li class="text-secondary">DC Kids</li>
-					<li class="text-secondary">DC Universe</li>
-					<li class="text-secondary">DC Power Visa</li>
+					<li class="text-secondary"><a href="#">DC</a></li>
+					<li class="text-secondary"><a href="#">MAD Magazine</a></li>
+					<li class="text-secondary"><a href="#">DC Kids</a></li>
+					<li class="text-secondary"><a href="#">DC Universe</a></li>
+					<li class="text-secondary"><a href="#">DC Power Visa</a></li>
 				</ul>
 			</div>
 			<div class="">
@@ -64,5 +64,10 @@ ul {
 
 img {
 	scale: 1.3;
+}
+
+a {
+	text-decoration: none;
+	color: #777;
 }
 </style>
